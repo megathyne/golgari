66bb787e7825899f714b01d92b81d7ea5176943e
Christos Benardis <megathyne@gmail.com>
2025-04-26 15:41:57 -0400

    actions

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -61,6 +61,46 @@ resource "aws_route_table_association" "public" {
 }
 
 # ECR
+# resource "aws_iam_role" "role" {
+#   name               = "ecr-role"
+#   assume_role_policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Principal": {
+#         "Service": ["ec2.amazonaws.com"]
+#       },
+#       "Action": "sts:AssumeRole"
+#     }
+#   ]
+# }
+# EOF
+# }
+
+# resource "aws_iam_policy" "policy" {
+#   name = "ecr-access-policy"
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = [
+#           "ecr:*",
+#         ]
+#         Effect   = "Allow"
+#         Resource = "*"
+#       },
+#     ]
+#   })
+# }
+
+# resource "aws_iam_policy_attachment" "attach" {
+#   name       = "ecr-attach"
+#   roles      = ["${aws_iam_role.role.name}"]
+#   policy_arn = aws_iam_policy.policy.arn
+# }
+
 resource "aws_ecr_repository" "golgari" {
   name                 = "golgari"
   image_tag_mutability = "MUTABLE"
